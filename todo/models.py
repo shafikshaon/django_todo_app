1d97e303cb463a5defc4981fbe38003de5089dbe
Shafikur Rahman Shaon <shafikshaon@gmail.com>
2019-10-22 20:15:31 +0600

    todo model add and migrations file add

--- todo/models.py
+++ todo/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+
+class Todo(models.Model):
+    title = models.CharField(max_length=128, null=False)
+    status = models.BooleanField(default=True)
+    created = models.DateTimeField(auto_now_add=True, editable=False)
+
+    class Meta:
+        app_label = 'todo'
+        ordering = ('-created',)
+
+    @property
+    def total_active_todo(self):
+        return self._meta.model.objects.filter(status=True).count()
+
+    @property
+    def total_inactive_todo(self):
+        return self._meta.model.objects.filter(status=False).count()
